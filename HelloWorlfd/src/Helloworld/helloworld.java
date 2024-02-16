475834ac522734f16404a3c2012822ba986427c0
javier.saugar <javier.saugar@AULA13-PC9.juanxxiii_23>
2024-02-16 20:33:15 +0100

    pito

---
+++ HelloWorlfd/src/Helloworld/helloworld.java
@@ -0,0 +1,18 @@
+/**file helloWorld
+ * 
+ */
+package Helloworld;
+
+/**clase principal para helloWorld
+ * 
+ */
+public class helloworld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	System.out.println("Hola mundo");
+}
+}
